0c2aec2e024f1f2de59471d5fa13b2879a82d3f1
Женя Вито z z z <evgenizef.ai@gmail.com>
2021-12-09 21:21:19 +1000

    загнал авто на парковку

--- LessonParking/Program.cs
+++ LessonParking/Program.cs
@@ -10,6 +10,18 @@ namespace LessonParking
     {
         static void Main(string[] args)
         {
+            var cars = new List<Car>()
+            {
+                new Car(){name ="Ford",Number = "A001AA01"},
+                new Car(){name ="Lada",Number = "B727ET77"}
+            };
+
+            var parking = new Parking();
+
+            foreach (var car in cars)
+            {
+                parking.Add(car);
+            }
         }
     }
 }
